d5f3acf9c32c045766971d0a84aa17f8621fce45
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-02-14 23:23:52 +0600

    case update part done

--- case.js
+++ case.js
@@ -1,8 +1,37 @@
-document.getElementById('btn-case-plus').addEventListener('click', function(){
+function updateCaseNumber(isIncrease){
     const caseNumberField =document.getElementById('case-number-field');
     const caseNumberString = caseNumberField.value ;
     const previousCaseNumber = parseInt(caseNumberString);
-
-    const newCaseNumber = previousCaseNumber + 1;
+    
+    let newCaseNumber;
+    if(isIncrease == true){
+        newCaseNumber = previousCaseNumber + 1;
+    }
+    else{
+        newCaseNumber = previousCaseNumber - 1;
+        
+    }
+     
     caseNumberField.value =newCaseNumber; 
-})
+    return newCaseNumber;
+}
+
+function updateCaseTotalPrice(newCaseNumber){
+    const caseTotalPrice = newCaseNumber * 59;
+
+    const caseTotalElement = document.getElementById('case-total');
+    caseTotalElement.innerText = caseTotalPrice;
+
+}
+
+document.getElementById('btn-case-plus').addEventListener('click', function(){
+    const newCaseNumber= updateCaseNumber(true);
+    updateCaseTotalPrice(newCaseNumber);
+    
+});
+document.getElementById('btn-case-minus').addEventListener('click', function(){
+    const newCaseNumber= updateCaseNumber(false);
+    updateCaseTotalPrice(newCaseNumber);
+    
+
+});
